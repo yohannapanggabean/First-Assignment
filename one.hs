13d15f39c727d38a15df2c00050e4ecfb9f9ad8b
yohannatp <yohannapanggabean.yp@gmail.com>
2015-09-08 17:51:43 +0700

    Second Task
    @squest @skadinyo

--- one.hs
+++ one.hs
@@ -30,9 +30,35 @@ min' x y
 
 --pembatas
 
+take' _ [] = []
+take' 0 (x:xs) = []
+take' n (x:xs) = x : take' (n-1) xs
 
+--pembatas
 
+drop' _ [] = []
+drop' 0 (xs) = xs
+drop' n (x:xs) = drop' (n-1) xs
 
+--pembatas
+
+head' (x:xs) = x
+
+--pembatas
+
+tail' (x:xs) = xs
+
+--pembatas
+
+last' [a,b] = b
+last' (x:xs) = last' (xs)
+
+--pembatas]
+
+init' [x] = []
+init' (x:xs) = x : init' xs
+
+--pembatas
 
 
 
