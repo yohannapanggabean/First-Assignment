32c35dd3fe81dc8c7df6e39dc28ab09305b9b233
yohannatp <yohannapanggabean.yp@gmail.com>
2015-09-08 10:56:12 +0700

    First Assignment

---
+++ one.hs
@@ -0,0 +1,90 @@
+module One where
+
+import Data.List
+
+--pembatas
+
+null' x
+  | x == [] = True
+  | otherwise = False
+
+--pembatas
+
+fst' (x,y) = x
+
+--pembatas
+
+snd' (x,y)= y
+
+--pembatas
+
+max' x y
+  | x > y = x
+  | x < y = y
+
+--pembatas
+
+min' x y
+  | x > y = y
+  | x < y = x
+
+--pembatas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- save
